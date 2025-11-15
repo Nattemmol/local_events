e0648942c5e3f1f008df10f894b32d7ee8e9a874
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2025-11-15 05:00:18 -0500

    Create triangle_sum.py

---
+++ triangle_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+        n = len(nums)
+        for _ in range(n-1):        
+            for i in range(len(nums)-1):
+                nums[i]=(nums[i] + nums[i+1])%10
+            nums.pop()
+        return nums[0]
